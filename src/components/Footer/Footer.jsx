3873004fd6efc36c74649d0c35c7a178a80a2daf
reza pratama <rezap9303@gmail.com>
2024-08-07 17:52:41 +0900

    first commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <div className="footer" id="footer">
+        <div className="footer-content">
+            <div className="footer-content-left">
+                <img src={assets.logo} alt="" />
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores dolorum alias deleniti sint eligendi ut.</p>
+                <div className="footer-secial-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <il>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Delivery</li>
+                    <li>Privacy policy</li>
+                </il>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>081248893566</li>
+                    <li>test@gmail.com</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">copyright</p>
+    </div>
+  )
+}
+
+export default Footer
